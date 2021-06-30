1be380b5de8deb9bc249b422f18bd78fa23df4b6
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-06-30 03:06:50 +0200

    impl onsubmit clear test

---
+++ testing/src/__tests__/CommentBox.test-.js
@@ -0,0 +1,41 @@
+import React from "react";
+import CommentBox from "components/CommentBox";
+import { mount } from "enzyme/build";
+import reactDom from "react-dom";
+
+let wrapped;
+beforeEach(() => {
+  wrapped = mount(<CommentBox />);
+});
+
+afterEach(() => {
+  wrapped.unmount();
+});
+
+// console.log(wrapped.find("textarea").length);
+
+it(" has a text area and a button", () => {
+  expect(wrapped.find("textarea").length).toEqual(1);
+  expect(wrapped.find("button").length).toEqual(1);
+});
+
+describe("the textarea", () => {
+  beforeEach(() => {
+    wrapped.find("textarea").simulate("change", {
+      target: { value: "my comment test value" },
+    });
+
+    wrapped.update();
+  });
+  it(" has a textarea that user can type in ", () => {
+    expect(wrapped.find("textarea").prop("value")).toEqual(
+      "my comment test value"
+    );
+  });
+
+  it(" cleares textarea on submit", () => {
+    wrapped.find("form").simulate("submit");
+    wrapped.update();
+    expect(wrapped.find("textarea").prop("value")).toEqual("");
+  });
+});
