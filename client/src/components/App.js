944c3f977d5ee32633ad92a8ffbc845183c6d4f4
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-07-05 01:54:32 +0200

    first commit

---
+++ client/src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+
+const App = () => {
+  return (
+    <div>
+      <Header></Header>
+      <h2>Hi, im the app</h2>
+    </div>
+  );
+};
+
+export default App;
