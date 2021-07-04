da25e1be4b5602a499c18348cfae19d28f8db35d
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-07-05 00:44:00 +0200

    impl / controller

--- auth/router.js
+++ auth/router.js
@@ -1,5 +1,13 @@
 const Authentication = require("./controllers/authentication");
+const passportService = require("./services/passport");
+const Passport = require("passport");
+const passport = require("passport");
+
+const requireAuth = passport.authenticate("jwt", { session: false });
 
 module.exports = (app) => {
+  app.get("/", requireAuth, (req, res) => {
+    res.send({ hi: "there " });
+  });
   app.post("/signup", Authentication.signup);
 };
