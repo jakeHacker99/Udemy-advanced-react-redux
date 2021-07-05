82c9c3f1b4169f77348f066f285b225228cc7d3b
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-07-05 02:09:01 +0200

    signup link

---
+++ client/src/components/auth/SignUp.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export default class SignUp extends Component {
+  render() {
+    return (
+      <form action="">
+        <fieldset>
+          <label htmlFor="">Email</label>
+        </fieldset>
+        <fieldset>
+          <label htmlFor="">password</label>
+        </fieldset>
+      </form>
+    );
+  }
+}
