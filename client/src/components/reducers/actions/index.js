58f45909613bf71fdd6dc42973237c9f02abe5be
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-07-05 19:36:29 +0200

    saving token to LS

--- client/src/components/reducers/actions/index.js
+++ client/src/components/reducers/actions/index.js
@@ -8,6 +8,7 @@ export const signup = (formProps, callback) => async (dispatch) => {
     });
 
     dispatch({ type: AUTH_USER, payload: response.data.token });
+    localStorage.setItem("token", response.data.token);
     callback();
   } catch (error) {
     dispatch({ type: AUTH_ERROR, payload: "Email in use" });
