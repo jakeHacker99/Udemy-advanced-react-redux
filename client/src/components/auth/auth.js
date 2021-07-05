99cac1e36db008f3d522137e5d6f83b68de4e24f
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-07-05 17:01:23 +0200

    wiring up redux store

---
+++ client/src/components/auth/auth.js
@@ -0,0 +1,8 @@
+const INITIAL_STATE = {
+  authenticated: "",
+  errorMessage: "",
+};
+
+export default (state = INITIAL_STATE, aciton) => {
+  return state;
+};
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,22 @@
 import React from "react";
 import reactDom from "react-dom";
+import { Provider } from "react-redux";
 import { BrowserRouter, Route } from "react-router-dom";
+import { createStore } from "redux";
 import App from "./components/App";
 import SignUp from "./components/auth/SignUp";
+import reducers from "./components/reducers";
 
 import Welcome from "./components/Welcome";
 
 reactDom.render(
-  <BrowserRouter>
-    <App>
-      <Route path="/" exact component={Welcome}></Route>
-      <Route path="/signup" exact component={SignUp}></Route>
-    </App>
-  </BrowserRouter>,
+  <Provider store={createStore(reducers, {})}>
+    <BrowserRouter>
+      <App>
+        <Route path="/" exact component={Welcome}></Route>
+        <Route path="/signup" exact component={SignUp}></Route>
+      </App>
+    </BrowserRouter>
+  </Provider>,
   document.querySelector("#root")
 );
