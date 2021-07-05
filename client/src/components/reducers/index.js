3544d52b230516c0aaac1c377e11e7a97a49b42c
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-07-05 17:15:44 +0200

    wire up redux store

--- client/src/components/reducers/index.js
+++ client/src/components/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
+import { reducer as formReducer } from "redux-form";
 import auth from "../auth/auth";
 
 export default combineReducers({
   auth,
+  form: formReducer,
 });
