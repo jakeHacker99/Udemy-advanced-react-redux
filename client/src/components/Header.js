3132d22527ec7552eefa90b4c7f66f55de16280b
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-07-06 15:39:05 +0200

    impl turnery, code cleanup

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,27 +3,25 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
 class Header extends Component {
-  renderLinks() {
-    if (this.props.authenticated) {
-      return (
-        <div>
-          <Link to="/signout"> Sign Out</Link>
-          <Link to="/feature"> Feature</Link>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <Link to="/signup"> Sign Up</Link>
-          <Link to="/signin"> Sign In</Link>
-        </div>
-      );
-    }
-  }
+  renderLinks() {}
   render() {
     return (
       <div className="header">
         <Link to="/"> Redux Auth</Link>
+        <>
+          {this.props.authenticated ? (
+            <div>
+              <Link to="/signout"> Sign Out</Link>
+              <Link to="/feature"> Feature</Link>
+            </div>
+          ) : (
+            <div>
+              <Link to="/signup"> Sign Up</Link>
+              <Link to="/signin"> Sign In</Link>
+            </div>
+          )}
+        </>
+
         {this.renderLinks()}
       </div>
     );
