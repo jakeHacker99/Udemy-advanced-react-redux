652b9e1ad0bb1bd45a9fccf083e210b7ea23a859
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-07-09 00:58:38 +0200

    adding router property to Authentication

--- Higher Order Components v14/src/components/require_auth.js
+++ Higher Order Components v14/src/components/require_auth.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 export default (ComposedComponent) => {
   class Authentication extends Component {
+    static contextType = {
+      router: React.PropTypes.object,
+    };
     render() {
+      console.log(this.props.authenticated);
       return <ComposedComponent {...this.props} />;
     }
   }
-  return Authentication;
+  const mapStateToProps = (state) => {
+    return { authenticated: state.authenticated };
+  };
+
+  return connect(mapStateToProps)(Authentication);
 };
