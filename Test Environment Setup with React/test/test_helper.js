9088c7d74a504cb03e39606ffa0632a8a15823af
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-07-08 16:40:00 +0200

    impl renderComponent

--- Test Environment Setup with React/test/test_helper.js
+++ Test Environment Setup with React/test/test_helper.js
@@ -1,5 +1,7 @@
 import jsdom from "jsdom";
 import jquery from "jquery";
+import TestUtils from "react-addons-test-utils";
+import ReactDOM from "react-dom";
 // setup env run as broswer in cmd
 global.document = jsdom.jsdom(
   "<!doctype html><html><body></body></body></html>"
@@ -8,6 +10,10 @@ global.window = global.document.defaultView;
 const $ = jquery(global.window);
 
 // build renderComponent helper
+renderComponent = (ComponentClass) => {
+  const componentInstance = TestUtils.renderIntoDocument(<ComponentClass />);
+  return $(ReactDOM.findDOMNode(componentInstance));
+};
 
 // build helper for simulate
 
