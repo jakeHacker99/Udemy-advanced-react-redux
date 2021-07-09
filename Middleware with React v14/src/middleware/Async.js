7c24e0a2b2594ee5972593f704df3295d35e2657
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-07-10 00:21:07 +0200

    lets impl some  cool auth for client

--- Middleware with React v14/src/middleware/Async.js
+++ Middleware with React v14/src/middleware/Async.js
@@ -1,12 +1,13 @@
 export default function ({ dispatch }) {
   return (next) => (action) => {
+    const handleResponse = (res) => {
+      const newAction = { ...action, payload: res };
+      dispatch(newAction);
+    };
     if (!action.payload || !action.payload.then) {
       next(action);
     }
 
-    action.payload.then((res) => {
-      const newAction = { ...action, payload: res };
-      dispatch(newAction);
-    });
+    action.payload.then(handleResponse);
   };
 }
