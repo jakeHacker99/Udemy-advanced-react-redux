6f85ab3491f80720933d6529253b428a7ff40d27
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-07-11 17:59:21 +0200

    finally fixed header logic

--- Client Side Authentication v14/src/components/header.js
+++ Client Side Authentication v14/src/components/header.js
@@ -1,13 +1,49 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
-export default class Header extends Component {
+class Header extends Component {
+  renderLink() {
+    if (this.props.authenticated) {
+      return (
+        <li className="nav-items">
+          <Link to="/signout" className="nav-link">
+            Sign Out
+          </Link>
+        </li>
+      );
+    } else {
+      return [
+        <li className="nav-item" key={1}>
+          <Link to="/signin" className="nav-link">
+            Sign In
+          </Link>
+        </li>,
+        <li className="nav-item" key={2}>
+          <Link to="/signup" className="nav-link">
+            Sign Up
+          </Link>
+        </li>,
+      ];
+    }
+  }
+
   render() {
     return (
       <nav className="navbar navbar-light">
-        <ul className="nav navbar-nav">
-          <li className="nav-item">Sign In</li>
-        </ul>
+        <Link to="/" className="navbar-brand">
+          Redux Auth
+        </Link>
+        <ul className="nav navbar-nav">{this.renderLink()}</ul>
       </nav>
     );
   }
 }
+
+export const mapStateToProps = (state) => {
+  return {
+    authenticated: state.auth.authenticated,
+  };
+};
+
+export default connect()(Header);
--- Client Side Authentication v14/src/reducers/index.js
+++ Client Side Authentication v14/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import { reducer as form } from "redux-form";
 import authReducer from "./auth_reducer";
+
+//add reducers here as you develop new ones
 const rootReducer = combineReducers({
   form,
   auth: authReducer,
