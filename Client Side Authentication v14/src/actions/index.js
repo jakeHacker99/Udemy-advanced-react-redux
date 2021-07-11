35d18fd9905f7d4459f8361d18ff5922b4b4805c
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-07-11 15:58:00 +0200

    Auth Error Messaging

--- Client Side Authentication v14/src/actions/index.js
+++ Client Side Authentication v14/src/actions/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { browserHistory } from "react-router";
 import * as actions from "../actions";
 
-import AUTH_USER from "../actions/types";
+import { AUTH_USER, AUTH_ERROR } from "../actions/types";
 const API_URL = "https:localhost:3090";
 export const signinUser = ({ email, password }) => {
   (dispatch) => {
@@ -15,7 +15,9 @@ export const signinUser = ({ email, password }) => {
         browserHistory.push("/feature");
         localStorage.setItem("token", res.data.token);
       })
-      .catch();
+      .catch(() => {
+        dispatch(authError("Invalid logininfo"));
+      });
 
     // if request is good..
     // update user state to auth
@@ -23,5 +25,12 @@ export const signinUser = ({ email, password }) => {
     // redirect to /feature
     // else
     // show  err
+  };
+};
+
+export const authError = (err) => {
+  return {
+    type: AUTH_ERROR,
+    payload: err,
   };
 };
