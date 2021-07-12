a37e7d722b89dde79c6761fc9293e7a939acf9ef
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-07-12 11:40:42 +0200

    form validation

--- Client Side Authentication v14/src/components/auth/signup.js
+++ Client Side Authentication v14/src/components/auth/signup.js
@@ -3,8 +3,61 @@ import { connect } from "react-redux";
 import { reduxForm } from "redux-form";
 import * as actions from "../../actions";
 
-export default class Signup extends Component {
+class Signup extends Component {
   render() {
-    return <div>Sign Up</div>;
+    const {
+      handleSubmit,
+      fields: { email, password, passwordConfirm },
+    } = this.props;
+
+    return (
+      <form>
+        <fieldset className="form-group">
+          <label>Email:</label>
+          <input className="form-control" {...email} />
+        </fieldset>
+        <fieldset className="form-group">
+          <label>Password:</label>
+          <input className="form-control" {...password} type="password" />
+        </fieldset>
+        <fieldset className="form-group">
+          <label>Confirm Password:</label>
+          <input
+            className="form-control"
+            {...passwordConfirm}
+            type="password"
+          />
+        </fieldset>
+        <button action="submit" className="btn btn-primary">
+          Sign Up!
+        </button>
+      </form>
+    );
   }
 }
+
+const validate = (formProps) => {
+  const errors = {};
+
+  if (!formProps.email) {
+    errors.email = "please enter an email";
+  }
+
+  if (!formProps.password) {
+    errors.password = "please enter a password";
+  }
+  if (!formProps.passwordConfirm) {
+    errors.passwordConfirm = "enter password again to confirms";
+  }
+
+  if (formProps.password !== formProps.passwordConfirm) {
+    errors.password = "password just match";
+  }
+  return errors;
+};
+
+export default reduxForm({
+  form: "signup",
+  fields: ["email", "password", "passwordConfirm"],
+  validate,
+})(Signup);
