a3b868356770f67402fdd4a2c188ec08ba8cb124
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-07-22 23:19:58 +0200

    init

---
+++ ecom-graphql/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h3>Ecom React Redux, GraphQL, Firebase</h3>
+      <button>click to run coolest app ever</button>
+    </div>
+  );
+}
+
+export default App;
